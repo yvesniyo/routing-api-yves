76bbdc8416da01853ad93f2554eccf25fd9cc157
Niyobuhungiro Yves <niyobuhungiro.yves@gmail.com>
2019-06-03 22:00:14 +0200

    Heroku

--- email.js
+++ email.js
@@ -1,51 +1,12 @@
-const nodeMailin = require("node-mailin");
- 
-/* Start the Node-Mailin server. The available options are:
- *  options = {
- *     port: 25,
- *     logFile: '/some/local/path',
- *     logLevel: 'warn', // One of silly, info, debug, warn, error
- *     smtpOptions: { // Set of options directly passed to simplesmtp.createServer(smtpOptions)
- *        SMTPBanner: 'Hi from a custom Node-Mailin instance',
- *        // By default, the DNS validation of the sender and recipient domains is disabled so.
- *        // You can enable it as follows:
- *        disableDNSValidation: false
- *     }
- *  };
- * parsed message. */
-nodeMailin.start({
-  port: 25
-});
- 
-/* Access simplesmtp server instance. */
-nodeMailin.on("authorizeUser", function(connection, username, password, done) {
-  if (username == "johnsmith" && password == "mysecret") {
-    done(null, true);
-  } else {
-    done(new Error("Unauthorized!"), false);
-  }
-});
- 
-/* Event emitted when a connection with the Node-Mailin smtp server is initiated. */
-nodeMailin.on("startMessage", function(connection) {
-  /* connection = {
-      from: 'sender@somedomain.com',
-      to: 'someaddress@yourdomain.com',
-      id: 't84h5ugf',
-      authentication: { username: null, authenticated: false, status: 'NORMAL' }
-    }
-  }; */
-  console.log("helll")
-  console.log(connection);
-});
- 
-/* Event emitted after a message was received and parsed. */
-nodeMailin.on("message", function(connection, data, content) {
-  console.log(data);
-  /* Do something useful with the parsed message here.
-   * Use parsed message `data` directly or use raw message `content`. */
-});
- 
-nodeMailin.on("error", function(error) {
-  console.log(error);
-});
+var express=require("express")
+
+var app=express();
+
+app.get("/",(req,res,next)=>{
+  console.log("user")
+  res.send("hello user")
+})
+var port=process.env.PORT || 1000
+app.listen(port,()=>{
+  console.log("the server is running on this port "+ port)
+})
---
+++ index.js
@@ -0,0 +1,125 @@
+const request=require("request")
+const express=require("express")
+const app=express();
+const usersController=require("./controllers/users")
+const bodyParser = require("body-parser");
+const mongoose = require("./config/database"); //database configuration
+var jwt = require("jsonwebtoken");
+const cors=require("cors")
+var ejs=require("ejs")
+var path=require("path")
+var session = require('express-session');
+var MongoDBStore = require('connect-mongodb-session')(session);
+var store = new MongoDBStore({
+  uri: 'mongodb://localhost:27017/connect_mongodb_session_test',
+  collection: 'mySessions'
+});
+var routingsUser=require("./routes/user")
+var routingsDashboard=require("./routes/dashboard")
+// var routingsSuper=require("./routes/super")
+
+
+
+app.set("view engine","ejs")
+app.set("secretKey", "moneything");
+app.set('views', path.join(__dirname, 'views'));
+
+
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({
+  secret: 'This is a secret',
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 2 * 1 // 2 hour
+  },
+  store: store,
+  resave: true,
+  saveUninitialized: true
+}));
+app.use("/userAuth",usersController)
+
+app.use("/dashboard",validateUser,checkLogin,routingsDashboard)
+app.use("/",validateUser,routingsUser)
+app.get("/logout",(req,res,next)=>{
+  req.session.destroy(()=>{
+    res.redirect("/")
+  })
+})
+
+app.use("/",express.static('views'))
+app.use("/userAuth",express.static('views'))
+app.use("/dashboard",express.static('views'))
+
+app.get("/map/:token",(req,res,next)=>{
+	//console.log(req.params.id)
+	request.get(`https://jsonplaceholder.typicode.com/todos`,(error,response,body)=>{
+		if(!error && response.statusCode == 200){
+			var locals=JSON.parse(body)
+			res.send(locals)
+		}
+	})
+})
+function checkLogin(req,res,next){
+  if(req.login){
+    next()
+  }else{
+    res.redirect("/")
+  }
+}
+function validateUser(req, res, next) {
+  jwt.verify(req.session.token, req.app.get("secretKey"), function(
+    err,
+    decoded
+  ) {
+    if (err) {
+      //res.redirect("/")
+      //res.json({ status: "error", message: err.message, data: null });
+      req.login=false;
+      next()
+    } else {
+      req.login=true
+      req.userId = decoded.id;
+      next();
+    }
+  });
+}
+
+
+
+store.on('error', function(error) {
+  console.log(error);
+});
+mongoose.connection.on("error",()=>{
+      console.log("MongoDB connection error")
+  }
+);
+mongoose.connection.on("connected",()=>{
+      console.log("MongoDB connection success")
+  }
+);
+app.use(function(req, res, next) {
+  res.render("404",{
+    title:"YRouting Page Not Found",
+    data:null,
+    login:req.login,
+  })
+  console.log("Page not Found")
+});
+
+app.use(validateUser,function(err, req, res, next) {
+  console.log(err);
+  //console.log("error")
+  if (err.status === 404)
+    res.render("404",{
+      titles:"YRouting Page Not Found",
+      data:null,
+      login:req.login,
+    })
+  else res.render("500",{title:"YRouting Server error ",data:null,login:req.login,}) //res.status(500).json({ message: "Something looks wrong :( !!!" });
+});
+var port=process.env.PORT || 1000;
+app.listen(port,()=>{
+	console.log("Server started on port "+port)
+})
+
